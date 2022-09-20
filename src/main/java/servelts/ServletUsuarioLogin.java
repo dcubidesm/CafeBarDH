552e876ac1ef1b3f589e9be27919b0bb54da9cc9
dcubidesm <111820300+dcubidesm@users.noreply.github.com>
2022-09-19 23:55:31 -0500

    Update ServletUsuarioLogin.java

--- src/main/java/servelts/ServletUsuarioLogin.java
+++ src/main/java/servelts/ServletUsuarioLogin.java
@@ -1,4 +1,3 @@
-//Es el comunicador entre JS (por parte de Front) y hacia la base de datos por IUC(Interfaz de Usuario Controller - IUsuarioController.java)
 package servlets;
 
 import java.io.IOException;
@@ -29,33 +28,26 @@ public class ServletUsuarioLogin extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         * Recibe la solictud o requerimiento del .JS , para devolverle una respuesta cuando se cumplan las dos condiciones y
-         * se utiliza doGet y doPost
-         *doGet obtiene la información del index.js 
-         *doPost muestra la información de index.js
-         * Seguido le manda esa información a los controladores 
 	 */
         @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
 		UsuarioController usuario = new UsuarioController();
-		String username = request.getParameter("usuario");//Campo user_name del backenda base de datos
-		String contrasena = request.getParameter("contrasena");//Campo passsword del backenda base de datos
+		String username = request.getParameter("username");
+		String contrasena = request.getParameter("contrasena");
 		String result = usuario.login(username, contrasena);
 		
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println(result);
-		out.flush();//temporalmente almacena esa info de contraseña y usuario y si alguna persona mas quiera loguearse, el flush puede desbordarse por tanta ocupacion, soltando y liberando la memoria para que otra persona pueda ingresar. 
-		out.close();//cierra el flush o si no queda como un ciclo
+		out.flush();
+		out.close();
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-        //doGet ontiene la informacion 
-        //doPost muestra la informacion
         @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
